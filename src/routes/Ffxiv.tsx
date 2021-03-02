a81b3a03cdffe4f274ad536f8196fae089f52183
Ibrahim <georgeipsum@gmail.com>
2021-03-02 15:28:15 -0500

    boilerplate

---
+++ src/routes/Ffxiv.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Ffxiv: React.FC = () => {
+  return <></>;
+}
+
+export default Ffxiv;
---
+++ src/routes/Qwerty.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Qwerty: React.FC = () => {
+
+  return <></>;
+}
+
+export default Qwerty;
